5b7440dd659832ec9fa2329787b84fa5b3e700b7
chanKR <107041844+chanKR-hub@users.noreply.github.com>
2024-03-03 22:39:59 +0900

    선택정렬
    선택정렬이란?
    - 선택 정렬 : 최솟값 또는 최댓값을 일일이 하나씩 찾아 앞으로 보내어 정렬하는 알고리즘 -- 시간복잡도 안좋으므로 거의 쓰이지 않는다고 함.
    
    예제) string 형식으로 숫자를 입력한 뒤 그 숫자들을 선택정렬 알고리즘으로 정렬(내림차순)하는 코드
    
    예시 :  "15423"을 입력받으면, 5 4 3 2 1 을 출력 
    
    배운점  : 
    1. substr() 함수를 통해서 문자열의 각 글자를 떼어내는 것.
    2. stoi() 함수를 통해서 문자열을 int형으로 변환하는 방법

--- main.cpp
+++ main.cpp
@@ -4,29 +4,34 @@
 using namespace std;
 
 int main(){
+    // 선택 정렬 : 최솟값 또는 최댓값을 일일이 하나씩 찾아 앞으로 보내어 정렬하는 알고리즘 -- 시간복잡도 안좋으므로 거의 쓰이지 않는다고 함.
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int N;
-    cin >> N;
-    vector<int> A (N,0);
+    string str;
+    cin >> str;
+    vector<int> A(str.length(),0); // 정렬할 수
 
-    for(int i=0; i < N; i++){
-        cin >> A[i];
+    for(int i = 0; i < str.length(); i++){
+        A[i] = stoi(str.substr(i,1));
     }
 
-    for(int i=0; i < N-1; i++){
-        for(int j=0; j < N-1; j++){
-            if(A[j] > A[j+1]){
-                int temp = A[j];
-                A[j] = A[j+1];
-                A[j+1] = temp;
+    for(int i = 0; i < str.length(); i++){
+        int max = i;
+        for(int j = i+1; j < str.length(); j++){
+            if(A[j] < A[max]){
+                max = j;
             }
         }
+        if(A[i] < A[max]){
+            int temp = A[i];
+            A[i] = A[max];
+            A[max] = temp;
+        }
     }
-
-    for(int i=0; i < N; i++){
+    for(int i=0; i < A.size(); i++){
         cout << A[i] << " ";
     }
+    return 0;
 }
